55374862704ce1868a5cfaf9828de5ff136a868a
Fredrik Fornwall <fredrik@fornwall.net>
2026-07-16 02:10:44 +0200

    Release 0.7.0

--- python/adbc_driver_spanner/_version.py
+++ python/adbc_driver_spanner/_version.py
@@ -1,4 +1,4 @@
 # Kept in sync with the crate version in Cargo.toml by cargo-release (see
 # pre-release-replacements there). CI overwrites this from the pushed vX.Y.Z
 # tag when building release wheels.
-__version__ = "0.6.0"
+__version__ = "0.7.0"
